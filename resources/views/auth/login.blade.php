c830ea12c30ae64fd6cf93abfd939d8ff9fdbf1b
Javier <javierprz22@gmail.com>
2018-09-18 18:09:13 -0500

    ultimos cambios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,92 +15,48 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!-- Styles -->
     <link rel="stylesheet" href={{asset('css/style.css')}}>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
 </head>
-<body >
-    <div >
-        <main>
-            <div class="container" >
-                <div class="row justify-content-lg-around align-items-center">
-                    <div class="px-lg-5 px-3 pt-5 col-lg-7 col order-lg-0 order-2 shadow text-light" style="background:#18386b;">
-                        <h1 class="h2 mb-4">Sistema Institucional de Información de Proyectos de Investigación.</h1>
-                        <h4 class="mb-4">¡Bienvenido!</h4>
-                        <div class="ml-lg-4 ml-2 mb-3">
-                            <h6 class="h6">Esta plataforma te ofrece servicios como:</h6>
-                            <li>Realizar el registro de tus Proyectos de Investigación</li>
-                            <li>Hacer seguimiento de las solicitudes</li>
-                            <li>Registrar Reportes Técnicos</li>
-                        </div>
-                        <p class="py-4" style="font-size:13px;">Si tienes alguna duda o sugerencia para mejorar nuestro servicio web, mandanos un correo a diai@unach.mx</p>
-                    </div>
-                    <div class="col-lg-4  mb-lg-0 mb-4 ">
-                        <div class="card shadow ">
-                           <div class="card card-header mb-2 p-0"  style="background-color:#d2a92d">
-                                <img class="ml-3" width="90%" src={{asset('img/posgrado.png')}} alt="">
-                           </div>
-            
-                            <div class="card-body">
-                                <form method="POST" action="{{ route('login') }}">
-                                    @csrf
+<body>
+  <div class="" style="width:90%; margin:auto; margin-top:7rem">
+    <div class="row" style="display:flex; justify-content:space-between;">
+      <div class="bg-accent">
+        <h4 class="color-white">Sistema Institucional de Información de Proyectos de Investigación.</h4>
+      </div>
+      <div class="col s7" style="margin-left:1.5rem">
+        <div class="card">
+          <div class="card-title left-align valing-wrapper">
+            <picture style="margin:10px;">
+              <img height="65px" src={{asset('img/logo_unach.png')}} alt="">
+            </picture>
+              <span>SIIPI - UNACH</span>
+          </div>
 
-                                        <div class="mb-3" >
-                                            <input id="email" type="email" placeholder={{ __('E-Mail Address') }} class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-            
-                                            @if ($errors->has('email'))
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $errors->first('email') }}</strong>
-                                                </span>
-                                            @endif
-                                        </div>
-                               
-            
-                                        <div class="mb-4">
-                                            <input id="password" placeholder={{ __('Password') }} type="password" class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-            
-                                            @if ($errors->has('password'))
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $errors->first('password') }}</strong>
-                                                </span>
-                                            @endif
-                                        </div>
-                        
-            
-                                    <div class="form-group pl-2">
-
-                                            <div class="form-check">
-                                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-            
-                                                <label class="form-check-label" for="remember">
-                                                    {{ __('Remember Me') }}
-                                                </label>
-                                            </div>
-                       
-                                    </div>
-            
-                                    <button type="submit" class="btn btn-block btn-outline-primary " >
-                                        {{ __('Login') }}
-                                    </button>
-
-                                        <div>
-                                            <a class="btn btn-link text-dark" href="{{ route('password.request') }}">
-                                                {{ __('Forgot Your Password?') }}
-                                            </a>
-
-                                            <a class="btn btn-link text-danger" href={{route('register')}}>
-                                                Crear una cuenta
-                                            </a>
-                                        </div>
-                                    
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+          <div class="card-content">
+            <div  style="margin-bottom:0px !important;" class="row">
+              <div class="input-field col s12">
+                <input id="email" type="email" class="validate">
+                <label for="email">Email</label>
+              </div>
             </div>
-        </main>
+            <div  style="margin-top:0px !important;"class="row">
+              <div class="input-field col s12">
+                <input id="password" type="password" class="validate">
+                <label for="password">Contraseña</label>
+              </div>
+            </div>
+            <div class="card-actions row">
+              <button class="btn waves-effect bg-primary col s12" type="button" name="button">Ingresar </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 </html>
